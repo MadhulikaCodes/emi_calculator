288cfca534990ebaf3e1cf3beb4c7472047294d6
madhulikaa223344 <madhulikaa223344@gmail.com>
2024-06-12 14:20:02 +0530

    EMI Caclculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,55 @@
 import React, { useState } from "react";
-import data from "./Data.js";
 
 const App = () => {
-  const [toggle, setToggle] = useState(0);
+  const [formData, setFormData] = useState({
+    principal: "",
+    interest: "",
+    year: "",
+  });
+  const [emi, setEmi] = useState(undefined);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    setEmi(
+      (formData.principal * formData.interest * (1 + formData.interest)) ^
+        (formData.year / ((1 + formData.interest) ^ (formData.year - 1)))
+    );
+  };
+
   return (
     <>
-      {data.map((item) => {
-        return (
-          <div key={item.id} className="container">
-            <div>
-              <span>{item.ques}</span>
-              <button
-                onClick={() =>
-                  toggle == item.id ? setToggle(0) : setToggle(item.id)
-                }
-              >
-               {toggle==item.id ?'^':'#'}
-              </button>
-            </div>
-            {toggle == item.id && <p>{item.ans}</p>}
+      <div className="container">
+        <span>EMI Calculator</span>
+        <form onSubmit={handleSubmit}>
+          <div className="input">
+            <p>Principal :</p>
+            <input
+              name="principal"
+              value={formData.principal}
+              onChange={handleChange}
+            />
           </div>
-        );
-      })}
+          <div className="input">
+            <p>Interest :</p>
+            <input
+              name="interest"
+              value={formData.interest}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="input">
+            <p>Year :</p>
+            <input name="year" value={formData.rate} onChange={handleChange} />
+          </div>
+          <button type="submit">Calculate</button>
+        </form>
+        {emi && <p>{`Your emi will be ${emi}`}</p>}
+      </div>
     </>
   );
 };
